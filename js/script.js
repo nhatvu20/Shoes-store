07747601ddee46f19c2b808b2aa5e925f1891a48
Nhat Vu <tlavu2003@gmail.com>
2023-02-01 01:08:09 +0700

    update auto show slide and dot slide

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let menubar=document.getElementById('menu-bar');
 let navbar=document.querySelector('.navbar');
-
+// navbar on mobie
 menubar.onclick=()=>{
   navbar.classList.toggle('active');
 }
@@ -9,23 +9,38 @@ window.onscroll=()=>{
   navbar.classList.remove('active');
 }
 
+let dots=document.querySelectorAll('.dot');
 let slides=document.querySelectorAll('.slider-container');
 let index=0;
 // console.log(slides);
 
+function currentSlide(n){
+  slides[index].classList.remove('active');
+  dots[index].classList.remove('dotActive');
+  index=n;
+  slides[index].classList.add('active');
+  dots[index].classList.add('dotActive');
+}
+
 function next(){
   slides[index].classList.remove('active');
+  dots[index].classList.remove('dotActive');
   index=(index+1)%slides.length;//2--> 2+1=3%3=0
   slides[index].classList.add('active');
+  dots[index].classList.add('dotActive');
 }
 
 function prev(){
   slides[index].classList.remove('active');
+  dots[index].classList.remove('dotActive');
   index=(index-1+slides.length)%slides.length; //0-1+3=2%3=2
   slides[index].classList.add('active');
+  dots[index].classList.add('dotActive');
 }
 
-
+// autoshowslide
+setInterval(next,7000);
+// Featured product
 document.querySelectorAll('.featured-image-1').forEach(image1=>{
   image1.addEventListener('click',()=>{
     var srcnew=image1.getAttribute('src');
@@ -46,52 +61,3 @@ document.querySelectorAll('.featured-image-3').forEach(image3=>{
     document.querySelector('.big-image-3').src=srcnew;
   })
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let slides = document.querySelectorAll('.slider-container');
-// let index=0;
-
-// function next(){
-//   slides[index].classList.remove('active');
-//   index=(index+1)%slides.length;// (2+1)%3=2
-//   slides[index].classList.add('active');
-// }
-
-// function prev(){
-//   slides[index].classList.remove('active');
-//   index=(index-1+slides.length)%slides.length;//0 --> 0-1+3=2 % 3 =2 
-//   slides[index].classList.add('active');
-// }
